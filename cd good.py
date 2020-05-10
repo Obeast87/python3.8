afdc0bb801b9e031d494819b2ade312247f53253
David Hendren <obeast@gmail.com>
2020-05-09 23:09:32 -0700

    Add files via upload

---
+++ cd good.py
@@ -0,0 +1,36 @@
+import webbrowser
+
+#name input
+name = input('What is your name?: ')
+if name == "":
+	print('''NO NAME
+EXITING PROGRAM''')
+	exit()
+
+#intro
+cd = input('''Welcome, ''' + name + '''! Do you prefer C or D? 
+(both upper and lowercase characters are okay): ''')
+
+#C/c input
+if cd == "C": 
+	print (name +' wants a print out of Oyster smiling.')
+	webbrowser.open('https://www.youtube.com/watch?v=maAFcEU6atk')
+
+if cd == "c": 
+	print (name +' wants a print out of Oyster smiling.')
+	webbrowser.open('https://www.youtube.com/watch?v=maAFcEU6atk')
+
+#D/d input
+elif cd == "D":
+	print(name + ' wants to meet that dad.')
+	webbrowser.open('https://www.youtube.com/watch?v=1g_UHV42jOA')
+
+elif cd == "d":
+	print(name + ' wants to meet that dad.')
+	webbrowser.open('https://www.youtube.com/watch?v=1g_UHV42jOA')
+
+
+#otherwise
+else:
+	print ('''INVALID INPUT
+EXITING PROGRAM''')
